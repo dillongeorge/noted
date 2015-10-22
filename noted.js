ed04e064f43ab3b665a4a562a4f23582f3ea412a
Dillon George <dr3george@gmail.com>
2015-10-22 16:48:18 -0700

    Add collection, users, and form for submitting to collection

--- noted.js
+++ noted.js
@@ -1,19 +1,31 @@
+Notes = new Mongo.Collection("notes");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.notes.helpers({
+    'note': function(){
+      return Notes.find({}, {sort: {createdOn: -1}});
     }
-  });
+  })
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.addNoteForm.events({
+    'submit form': function(event){
+      event.preventDefault();
+
+      var noteTitle = event.target.noteTitle.value;
+      var noteContent = event.target.noteContent.value;
+      var user = Meteor.userId();
+
+      Notes.insert({
+        title: noteTitle,
+        content: noteContent,
+        createdBy: user,
+        createdOn: new Date()
+      });
+
+      event.target.noteTitle.value = "";
+      event.target.noteContent.value = "";
     }
-  });
+  })
 }
 
 if (Meteor.isServer) {
