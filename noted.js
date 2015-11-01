bf762f40818cc891b6a9dce0cb2c9d197d98f934
Dillon George <dr3george@gmail.com>
2015-11-01 01:49:53 -0700

    Packery when notes are edited

--- noted.js
+++ noted.js
@@ -4,9 +4,21 @@ Images = new FS.Collection("images", {
   stores: [new FS.Store.GridFS("images")]
 });
 
+var triggerPackeryLayout = function(){
+  var $container = $('#notes-container');
+
+  $container.packery({
+    itemSelector: '.note',
+    gutter: 10
+  });
+};
+
 if (Meteor.isClient) {
 
   /**** Notes template ********/
+  Meteor.startup(function(){
+
+  });
 
   Template.notes.helpers({
     'note': function(){
@@ -14,33 +26,75 @@ if (Meteor.isClient) {
     },
     'noteImage': function(){
       return Images.findOne(this.imageId._id);
+    },
+    'rendered': function(){
+      console.log("rendered");
+      $('#notes-container').packery();
     }
   });
 
   Template.notes.onRendered(function(){
-
-    var triggerPackeryLayout = function(){
-      var $container = $('#notes-container');
-
-      $container.packery({
-        itemSelector: '.note',
-        gutter: 10
-      });
-    };
-
     this.find('#notes-container')._uihooks = {
       insertElement: function(node, next){
         triggerPackeryLayout();
         $('#notes-container').prepend(node);
         $('#notes-container').packery('prepended', node);
       }
-    }
+    };
   });
 
   Template.notes.events({
     'click .note-content': function(event){
       event.preventDefault();
-      $(this).lightbox();
+      var element = event.currentTarget;
+      var self = this;
+      var container = $('<div>').addClass("update-form-container");
+      var title = $('<input id="update-note-title" type="text" placeholder="Title">')
+        .addClass("form-control")
+        .addClass("borderless")
+        .addClass("update-note-title")
+        .val(self.title);
+
+      var content = $('<textarea id="update-note-content" placeholder="Note">')
+        .addClass("form-control")
+        .addClass("borderless")
+        .addClass("update-note-content")
+        .val(self.content);
+
+      var updateNoteSubmit = function(){
+        Meteor.call(
+            "updateNote",
+            self._id,
+            $('#update-note-title').val(),
+            $('#update-note-content').val(),
+          );
+          $.fancybox.close();
+      };
+
+      var submitButton = $('<input type="button" id="update-note-button" value="Submit">')
+      .click(updateNoteSubmit);
+
+      var submit = $('<div>')
+        .addClass("update-note-options-container")
+        .append(submitButton);
+
+      container.append(title)
+        .append(content)
+        .append(submit);
+
+      $.fancybox.open(container, {
+                maxWidth  : 800,
+                maxHeight : 600,
+                fitToView : false,
+                width   : '70%',
+                height    : '70%',
+                autoSize  : false,
+                closeClick  : false,
+                openEffect  : 'none',
+                closeEffect : 'none',
+                padding: 0,
+                closeBtn: false
+      });
     },
     'click .note-options': function(){
       console.log("Icon");
@@ -70,13 +124,15 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    
-  });
 
   Meteor.methods({
-    'updateNote': function(id, photoId){
-      Notes.update(id, {$set: {imageId: photoId}});
+    'updateNote': function(id, noteTitle, noteContent){
+        Notes.update({_id: id}, {$set:
+          {
+            title: noteTitle, 
+            content: noteContent
+          }
+      });
     },
     'insertNote': function(noteTitle, noteContent, pid){
       Notes.insert({
@@ -86,7 +142,6 @@ if (Meteor.isServer) {
         imageId: pid,
         createdOn: new Date()
       });
-      console.log("Made it");
     },
     'fetchImageUrl': function(imageId){
       return {url: Images.findOne(imageId).url()};
@@ -94,12 +149,9 @@ if (Meteor.isServer) {
   });
 
   Images.allow({
-    insert: function(){
-      return true;
-    },
-    update: function(){
-      return true;
-    },
-    download: function(){ return true; }
+    insert: function(){ return true; },
+    update: function(){ return true; },
+    download: function(){ return true; },
+    remove: function(){ return true; }
   })
 }
